8fae5727eabea341d1f9963e7eed8d61dbd0f5c4
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-13 12:12:24 -0700

    placeholder password working

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var special = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+	passwordText.value = password;
 
+}
+function generatePassword()
+{
+	return "password";
 }
 
 // Add event listener to generate button
