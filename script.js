222c2cd6552f7d4ba2286e4973c2d2260c0ff751
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-13 14:19:52 -0700

    verify at least one char type

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numeric = "0123456789";
-var special = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numeric = "0123456789";
+const special = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 // Write password to the #password input
 function writePassword() {
@@ -19,22 +19,41 @@ function writePassword() {
 function getChoices()
 {
 	var length = parseInt( prompt("How long?") );
-	if( length > 128 || length < 8 )
+	while( length > 128 || length < 8 )
 	{
-		alert("Between 8 and 128, my dude");
+		alert("Between 8 and 128, my dude.");
 		length = prompt("How long?");
 	}
 
-	var lower = prompt("Lower case characters?");
-	var upper = prompt("Upper case characters?");
-	var numeric = prompt("Numbers?");
-	var special = prompt("Special characters?");
-	return { lower, upper, numeric, special };
+	var lower = confirm("Include lowercase characters?");
+	var upper = confirm("Include uppercase characters?");
+	var numeric = confirm("Include numbers?");
+	var special = confirm("Include special characters?");
+	if( !lower && !upper && !numeric && !special)
+	{
+		alert("You must select at least one character type.");
+		return getChoices();
+	}
+	return { length, lower, upper, numeric, special };
 }
 function generatePassword(userChoices)
 {
-	return JSON.stringify(userChoices);
+	var passwordChars = determineRange(userChoices);
+	return passwordChars;
 	//return "password" + userChoices;
+}
+function determineRange(userChoices)
+{
+	var chars = "";
+	if( userChoices.lower )
+		chars += lowercase;
+	if( userChoices.upper)
+		chars += uppercase;
+	if( userChoices.numeric )
+		chars += numeric;
+	if( userChoices.special )
+		chars += special;
+	return chars;
 }
 
 // Add event listener to generate button
