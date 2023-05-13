5b1c117316e0d15faceb7bf275c8153073596137
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-13 13:27:23 -0700

    choices object propagating

--- script.js
+++ script.js
@@ -3,21 +3,38 @@ var generateBtn = document.querySelector("#generate");
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "0123456789";
-var special = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
+var special = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 // Write password to the #password input
 function writePassword() {
-	var number = parseInt( prompt("pick a number or something") );
-	var password = generatePassword();
-	password += number;
+	var choices = getChoices();
+	//var number = parseInt( prompt("pick a number or something") );
+	var password = generatePassword(choices);
+	//password += choices;
 	var passwordText = document.querySelector("#password");
 
 	passwordText.value = password;
 
 }
-function generatePassword()
+function getChoices()
 {
-	return "password";
+	var length = parseInt( prompt("How long?") );
+	if( length > 128 || length < 8 )
+	{
+		alert("Between 8 and 128, my dude");
+		length = prompt("How long?");
+	}
+
+	var lower = prompt("Lower case characters?");
+	var upper = prompt("Upper case characters?");
+	var numeric = prompt("Numbers?");
+	var special = prompt("Special characters?");
+	return { lower, upper, numeric, special };
+}
+function generatePassword(userChoices)
+{
+	return JSON.stringify(userChoices);
+	//return "password" + userChoices;
 }
 
 // Add event listener to generate button
